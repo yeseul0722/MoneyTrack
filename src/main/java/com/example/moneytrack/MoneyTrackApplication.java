86bacbc7fea4287cdd555a27b76f7d092d4698c3
yeseul0722 <pdy051257@naver.com>
2025-03-19 10:25:02 +0900

    초기설정 완료

---
+++ src/main/java/com/example/moneytrack/MoneyTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moneytrack/MoneyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
