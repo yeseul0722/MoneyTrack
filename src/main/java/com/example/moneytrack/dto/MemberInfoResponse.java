91f14e22c3f27f6c17f835e9c80ba937e4c9669f
yeseul0722 <pdy051257@naver.com>
2025-05-04 02:17:29 +0900

    Refactor: 회원 정보에 계좌 추가

--- src/main/java/com/example/moneytrack/dto/MemberInfoResponse.java
+++ src/main/java/com/example/moneytrack/dto/MemberInfoResponse.java
@@ -1,9 +1,12 @@
 package com.example.moneytrack.dto;
 
+import com.example.moneytrack.domain.Account;
 import com.example.moneytrack.domain.Member;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class MemberInfoResponse {
@@ -11,20 +14,27 @@ public class MemberInfoResponse {
     private final String name;
     private final String gender;
     private final LocalDate dateOfBirth;
+    private final List<String> accountNumbers;
 
-    private MemberInfoResponse(String email, String name, String gender, LocalDate dateOfBirth) {
+    private MemberInfoResponse(String email, String name, String gender, LocalDate dateOfBirth, List<String> accountNumbers) {
         this.email = email;
         this.name = name;
         this.gender = gender;
         this.dateOfBirth = dateOfBirth;
+        this.accountNumbers = accountNumbers;
     }
 
     public static MemberInfoResponse from(Member member) {
+        List<String> accountNumbers = member.getAccounts().stream()
+                .map(Account::getAccountNumber)
+                .collect(Collectors.toList());
+
         return new MemberInfoResponse(
                 member.getEmail(),
                 member.getName(),
                 member.getGender(),
-                member.getDateOfBirth()
+                member.getDateOfBirth(),
+                accountNumbers
         );
     }
 }
